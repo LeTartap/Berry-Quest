196f0dda5ad1dfb368a05f449ffe6dca7c205094
David Galati <36513015+LeTartap@users.noreply.github.com>
2018-04-14 13:28:43 +0300

    Removed gun mechanics

--- Ceva important/Assets/Scripts/PlayerController.cs
+++ Ceva important/Assets/Scripts/PlayerController.cs
@@ -10,26 +10,14 @@ public class PlayerController : MonoBehaviour
     public float gravityScale;
     public float verticalVelocity;
     private float jumpForce = 10.0F;
-    //private Vector3 movement = Vector3.zero;//tbd
+
+
 
     public Animator anim;
-
-    public GameObject bullet;
-
-
-    private Vector3 worldpos;
-    private float mouseX;
-    private float mouseY;
-    private float cameraDif;
-    public Camera camera;
-
-    public GameObject fpc;
 
     void Start()
     {
         //Cursor.visible = false;
-        camera.enabled = false;
-        cameraDif = camera.transform.position.y - fpc.transform.position.y;
     }
     void Update()
     {
@@ -38,24 +26,14 @@ public class PlayerController : MonoBehaviour
         //Jump
         if (controller.isGrounded)
         {
-            verticalVelocity = -gravity * Time.deltaTime *gravityScale;
+            verticalVelocity = -gravity * Time.deltaTime * gravityScale;
             if (Input.GetKeyDown(KeyCode.Space))
-                verticalVelocity = jumpForce;          
+                verticalVelocity = jumpForce;
         }
         else
         {
-            verticalVelocity -= gravity * Time.deltaTime *gravityScale;
+            verticalVelocity -= gravity * Time.deltaTime * gravityScale;
         }
-
-        mouseX = Input.mousePosition.x;
-
-        mouseY = Input.mousePosition.y;
-
-        worldpos = camera.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
-
-        Vector3 turretLookDirection = new Vector3(worldpos.x, fpc.transform.position.y, worldpos.z);
-
-        fpc.transform.LookAt(turretLookDirection);
 
         //Movement
 
@@ -66,23 +44,13 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0F, moveVertical);
         movement.Normalize();
         //transform.rotation = Quaternion.LookRotation(movement);
-                
+
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
         controller.Move(moveVector * Time.deltaTime);
 
-        
-
-        // projectile
-        if (Input.GetButtonDown("Fire1"))
-        {
-            bullet.transform.position=Vector3.Lerp(bullet.transform.position, new Vector3())
-        }
-
-
-
         anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
 
-        anim.SetBool("isGrounded", (controller.isGrounded));
-        
+
+
     }
 }
