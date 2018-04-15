0dd3e245a71a6af87f8d2ffb60ea3156db813691
MihuiAnghelin <mihai.anghelin@gmail.com>
2018-04-15 20:20:11 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/LeTartap/Mr-Bonesacks-s-test-world""
    This reverts commit 9037dbb58821f49d5d4697068931e553eb1c049f.

---
+++ Ceva important/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    private Rigidbody myRB;
+    public float movespeed;
+    public PlayerController thePlayer;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRB = GetComponent<Rigidbody>();
+        thePlayer = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(thePlayer.transform.position);	
+	}
+}
