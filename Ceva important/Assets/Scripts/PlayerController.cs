6f4082673966bfe293b6de7c980a05fa19747e5d
MihuiAnghelin <mihai.anghelin@gmail.com>
2018-04-15 15:08:53 +0300

    Revert "Revert "New Movement Script + FaceDirection""
    This reverts commit a365a7c47d7127b25eac32f73e24d2ca9fea6338.

--- Ceva important/Assets/Scripts/PlayerController.cs
+++ Ceva important/Assets/Scripts/PlayerController.cs
@@ -2,67 +2,56 @@
 
 public class PlayerController : MonoBehaviour
 {
-    #region VariablesDeclaration
-    CharacterController controller;    
-    public float verticalVelocity;
-    public float gravity;
-    public float jumpForce;
-    public float moveSpeed;
+    #region VariablesDeclaration     
+    public float verticalVelocity = 10.0f;
+    public float gravity = 20.0f;
+    public float jumpForce = 5.0f;
+    public float moveSpeed = 6.0f;
+    private Rigidbody myRigidbody;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Camera mainCamera;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     private float bulletTimeStamp;
     public Animator anim;
     #endregion
-    #region FaceFollowCursorDeclaration
-    private Vector3 worldpos;
-    private float mouseX;
-    private float mouseY;
-    private float cameraDif;
-    public Camera pivot;
-    #endregion
+
 
     void Start()
     {
-        pivot.enabled = false;
-        cameraDif = pivot.transform.position.y - transform.position.y;
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
     }
-    void LateUpdate()
+    void Update()
     {
-        controller = GetComponent<CharacterController>();
-
         #region Jumping
-        if (controller.isGrounded)
+        //if (controller.isGrounded)
+        //{
+        //    verticalVelocity = -gravity * Time.deltaTime;
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //        verticalVelocity = jumpForce;
+        //}
+        //else
+        //{
+        //    verticalVelocity -= gravity * Time.deltaTime;
+        //}
+        #endregion
+
+        #region Movement + FaceFollowCursor
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (groundPlane.Raycast(cameraRay, out rayLength))
         {
-            verticalVelocity = -gravity * Time.deltaTime;
-            if (Input.GetKeyDown(KeyCode.Space))
-                verticalVelocity = jumpForce;
-        }
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime;
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
         }
         #endregion
-
-        #region Movement
-        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0F, moveVertical);
-        movement.Normalize();
-
-        controller.Move(movement * moveSpeed * Time.deltaTime);
-        controller.Move(moveVector * Time.deltaTime);
-        #endregion
-
-        #region FaceFollowCursor
-        mouseX = Input.mousePosition.x;
-        mouseY = Input.mousePosition.y;
-        worldpos = pivot.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
-        Vector3 turretLookDirection = new Vector3(worldpos.x, transform.position.y, worldpos.z);
-        transform.LookAt(turretLookDirection);
-        #endregion
-
+        
         #region Bullet
         if (bulletTimeStamp <= Time.time)
             if (Input.GetButtonDown("Fire1"))
@@ -73,16 +62,20 @@ public class PlayerController : MonoBehaviour
         #endregion
 
         #region Animations
-        anim.SetBool("isGrounded", Input.GetKey(KeyCode.Space));
+        //anim.SetBool("isGrounded", myRigidbody.isGrounded && Input.GetKey(KeyCode.Space));
         #endregion
+    }
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
     }
     void Fire()
     {
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);
+        //Create the Bullet from the Bullet Prefab
+       var bullet = (GameObject)Instantiate(
+           bulletPrefab,
+           bulletSpawn.position,
+           bulletSpawn.rotation);
 
         // Add velocity to the bullet
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * -10;
