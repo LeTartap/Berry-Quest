9550b16a59ed28f6cac6f3fa830f254e54cf343a
MihuiAnghelin <mihai.anghelin@gmail.com>
2018-04-16 23:48:14 +0300

    Added some animations
    #stillWorkingAtIt

--- Ceva important/Assets/Scripts/PlayerController.cs
+++ Ceva important/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     #endregion
 
-
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
@@ -34,17 +33,64 @@ public class PlayerController : MonoBehaviour
             Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
             transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
         }
-        #endregion
-
-        #region Animations
-
+        Quaternion rot = transform.rotation;
+        Vector3 v = rot.ToEulerAngles();
+        UpdateAnimation(moveInput, v);
         #endregion
     }
     void FixedUpdate()
     {
         myRigidbody.velocity = moveVelocity;
     }
-    
-    
-    
+    #region Animations    
+    void UpdateAnimation(Vector3 dir, Vector3 rotation)
+    {
+        if (dir.x == 0f && dir.z == 0f)
+        {
+            //Idle Animation
+            anim.SetBool("Movement", false);
+        }
+        else
+        {
+            anim.SetBool("Movement", true);
+        }
+        if (rotation.y >= -57 && rotation.y <= 57)//North
+        {
+            anim.SetFloat("North", 1f);
+            anim.SetFloat("Est", 0f);
+            anim.SetFloat("South", 0f);
+            anim.SetFloat("West", 0f);
+            anim.SetFloat("DirX", dir.x);
+            anim.SetFloat("DirZ", dir.z);
+        }
+        else if (rotation.y >= 57 && rotation.y <= 123)//Est
+        {
+            anim.SetFloat("North", 0f);
+            anim.SetFloat("Est", 1f);
+            anim.SetFloat("South", 0f);
+            anim.SetFloat("West", 0f);
+            anim.SetFloat("DirX", dir.x);
+            anim.SetFloat("DirZ", dir.z);
+        }
+        else if (rotation.y >= -123 && rotation.y <= 123)//South
+        {
+            anim.SetFloat("North", 0f);
+            anim.SetFloat("Est", 0f);
+            anim.SetFloat("South", 1f);
+            anim.SetFloat("West", 0f);
+            anim.SetFloat("DirX", dir.x);
+            anim.SetFloat("DirZ", dir.z);
+        }
+        else //West
+        {
+            anim.SetFloat("North", 0f);
+            anim.SetFloat("Est", 0f);
+            anim.SetFloat("South", 0f);
+            anim.SetFloat("West", 1f);
+            anim.SetFloat("DirX", dir.x);
+            anim.SetFloat("DirZ", dir.z);
+        }
+
+    }
+    #endregion
 }
