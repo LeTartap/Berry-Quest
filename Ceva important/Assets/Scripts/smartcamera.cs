11d48b7f7be7816e24531a5cab8a57c1bf669356
meemknight <36445656+meemknight@users.noreply.github.com>
2018-04-16 18:52:49 +0300

    urs

---
+++ Ceva important/Assets/Scripts/smartcamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smartcamera : MonoBehaviour {
+
+    float height;
+    public float speed;
+    public bool coliding = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        height = transform.position.y;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+  
+    private void OnTriggerEnter ()
+    {
+        coliding = true;
+        Vector3 vect= transform.position;
+        vect.y += speed * Time.deltaTime;
+        transform.position = vect;
+    }
+
+}
