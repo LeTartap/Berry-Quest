ce1ffccbdc19d4c71d353a7ef79738c4eaeb948f
David Galati <36513015+LeTartap@users.noreply.github.com>
2018-04-22 00:37:23 +0300

    mihai menu

--- Ceva important/Assets/Scripts/PlayerHealthManager.cs
+++ Ceva important/Assets/Scripts/PlayerHealthManager.cs
@@ -1,37 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 public class PlayerHealthManager : MonoBehaviour {
-    public int startingHealth=100;
+    public int startingHealth;
     public int currentHealth;
 
-    [Header("Unity Stuff")]
-    public Image healthbar;
-
-    //public GameObject healthbar;
-
 	// Use this for initialization
-	void Start ()
-    {
+	void Start () {
         currentHealth = startingHealth;
 	}
-    // Update is called once per frame
-    public void HurtPlayer(int damageAmount)
-    {
-        currentHealth -= damageAmount;
-        healthbar.fillAmount = (float)currentHealth / (float)startingHealth;
+	// Update is called once per frame
+	void Update () {
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
         }
 
     }
-    void Update ()
+    public void HurtPlayer(int damageAmount)
     {
-     
-
-
+        currentHealth -= damageAmount;
+        
     }
-
 }
