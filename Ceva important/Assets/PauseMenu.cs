c9140007dcaa9e7e93212a040273df81c83c9ede
MihuiAnghelin <mihai.anghelin@gmail.com>
2018-04-22 16:51:29 +0300

    Added InGame PauseMenu

---
+++ Ceva important/Assets/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public Bullet Bullet;
+    public PlayerController PlayerController;
+
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+                
+            }
+        }
+	}
+
+    public void Resume ()
+    {
+        
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Bullet.enabled = true;
+        PlayerController.enabled = true;
+    }
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+        Bullet.enabled = false;
+        PlayerController.enabled = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
