95b4999cf99977b5e684002ed01329fefb5865d6
meemknight <36445656+meemknight@users.noreply.github.com>
2018-05-01 10:35:07 +0300

    UI
    un pic de ui

--- Ceva important/Assets/Scripts/HUDCounter.cs
+++ Ceva important/Assets/Scripts/HUDCounter.cs
@@ -9,13 +9,13 @@ public class HUDCounter : MonoBehaviour {
     public murecounter mc;
     // Use this for initialization
     void Start () {
-        berryCountText.text = "Berries: " + mc.countmure.ToString();
+        berryCountText.text =  mc.countmure.ToString();
     }
     
     // Update is called once per frame
     void Update ()
     {
         berryCountText.text = mc.countmure.ToString();
-        killCountText.text = "Kills: " + killcounter.killcount.ToString();
+        killCountText.text =  killcounter.killcount.ToString();
     }
 }
